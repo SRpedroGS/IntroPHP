f8603a009782d4ca0e2b1983c125327a316c1882
SR_pedro_GS <125514911+SRpedroGS@users.noreply.github.com>
2024-04-12 21:08:29 -0300

    Add files via upload

---
+++ 3_LACOS_REPETICAO/4-foreach.php
@@ -0,0 +1,18 @@
+<hr>
+
+<?php
+$meses = array(
+    'Janeiro' => 1,
+    'Feveireiro' => 2,
+    'Março' => 3,
+    'Abril' => 4,
+);
+foreach($meses as $chave => $valor){
+    echo ' O mês ' . $chave . ' e ' . $valor . ' <br> ';
+}
+
+
+
+
+
+?>
